4a3224c87b985a99dc77b9d3d39330f640aecfb2
julietamponti <jponti@fi.uba.ar>
2021-09-23 20:45:01 -0300

    Se crean los lavados

--- desarrollo.py
+++ desarrollo.py
@@ -16,8 +16,57 @@ def crearDiccionario(archivo):
     archivo.close()
     return dic
 
+def mayorTiempoDeLavado(dic): #devuelve el numero de prenda que mas tarda den lavarse de las que quedan sin lavar y todos sus datos
+    max = 0 #ya que comparare con el y siempre sera menor a los tiempos de lavado
+    kmax = ''
+
+    for k in dic.keys():
+        if dic[k]['tiempo'] > max:
+            max = dic[k]['tiempo'] # toma el valor maximo de tiempo
+            kmax = k              # la prenda que mas tarda en lavarse
+    return (kmax,dic.pop(kmax)) # elimino la prenda, ya que ya ha sido asignada a un lavado, es decir que sera lavada
+
+
+def esCompatible(lista,dic):
+    compatible = True
+    index = 0
+    while index < len(lista) and compatible:    #la condicion de que sea compatible la prenda que se esta buscando localizar, es que sea compatible con   
+                                                #todas las prendas que ya estan asignadas al lavado        
+        
+        if lista[index] in dic['incomp']:      
+            compatible = False                  #si la prenda del lavado que esta iterando esta en la lista de incompatibles de la prenda que 
+                                                #se quiere asignar, ya no son incompatibles
+        
+        else:
+            index += 1
+    return compatible
+
+def crearLavados(lavados,prendas):  #devuelve un diccionario con todos 
+    while len(prendas) > 0:
+        lavado =[]
+        k, prenda = mayorTiempoDeLavado(prendas)
+        lavarropas = {}   
+        lavado.append(k)
+
+        for p,v in prendas.items():
+            if esCompatible(lavado,v):
+                lavado.append(p)
+        
+        for x in lavado:
+            lavarropas[k] = prenda
+            if x != k:
+                lavarropas[x] = prendas.pop(x) #idem pop anterior, se le asigno un lavado, por lo que ya no me interesa ubicarla en otro
+        
+        lavados[len(lavados)+1] = lavado   
+
+    return(lavados)
+
 def main():
     archivo = open('Enunciado.txt',mode= 'r',encoding= 'utf-8')
     prendas = crearDiccionario(archivo)
-    print(prendas)
+    #print(prendas)
+    lavados = {}
+    lavados = crearLavados(lavados,prendas)
+    print(lavados)
+
 main()
